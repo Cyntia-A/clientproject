ed5bf932eff4d473a9b209eb2ab1fee289caa063
Cintia Arambula <cintiaarambula@ccpaedu.com>
2019-04-26 01:15:36 +0000

    Update

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-# Project Name
+# Project Name: Book Website(Change Later)
 
-## Team Members
+## Team Members: Jenifer Lopez, Bremelyn Duran, & Cyntia Arambula
 
-## Problem
+## Problem: 
 
-## Opportunity
+## Opportunity: 
 
 ## Technical Magic
 
 ## Future Vision
+
+// key: MDzV6NduBDuiaCDiKqS3Q
+// secret: 0QodLlCzj3c6iKPyBI279mQPtSmZcuZbGD1JuYK1Q
+
+https://www.googleapis.com/books/v1/volumes?q={holly%20black}
--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
 /* global $ */
 
-console.log('hi');
+// console.log('hi');
 
-$('h1').click(function(){
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+// $('h1').click(function(){
+//     $('h1').css('color', 'green');
+//     $('h1').text('Ready to Code');
+// });
+
+$('#searchButton').click(function(){
+    
+    var searchTerm = $('#searchInput').val();
+    var request_URL = "https://www.googleapis.com/books/v1/volumes?q={" + searchTerm + "}";
+    
+    $.ajax({
+       url: request_URL,
+       method: "GET",
+       type: String, 
+       success: function(response){
+        
+        for(var i = 0; i < response.items.length; i++)
+        {
+            var bookTitle = "<p>" + response.items[i].volumeInfo.title + "</p>";
+            var bookAuthor = "<p>" + response.items[i].volumeInfo.authors + "</p>";
+            var bookImage = '<img src = "' + response.items[i].volumeInfo.imageLinks.thumbnail + '"/>';
+            
+            $("#mainScreen").append("<div>" + bookImage + bookTitle + bookAuthor + "</div>");
+        }   
+       
+       },
+        fail: function(error){
+            console.log(error);
+        }
+    });
+    
 });
